060a0d1327b2b99d40a4ae275c29864e4cfd5aa9
oguz <oguz.tanrikulu@metu.edu.tr>
2015-12-11 22:44:34 +0200

    image

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ class Image < ActiveRecord::Base
 
   belongs_to :imageable, :polymorphic => true
 
-  has_attached_file :imagefile, :styles => { :small => "320x280!", :original => '800x1000!' },
+  has_attached_file :imagefile, :styles => { :small => "320x320!", :original => '800x1000!' },
     :url => "/image/:hash.:extension", :hash_secret => ":id", :default_url => "/resources/images/default.png"
 
   validates_attachment :imagefile, content_type: { content_type: ['image/jpeg', 'image/png'] }, :size => { :in => 0..2.megabytes }
