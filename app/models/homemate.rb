2c1adca57bed9b4805efb78a57ff03c4351f4b0a
ONUR <onurdemirtas91@gmail.com>
2016-01-27 18:16:01 +0200

    tt

--- app/models/homemate.rb
+++ app/models/homemate.rb
@@ -1,42 +1,108 @@
 class Homemate < ActiveRecord::Base
-  
+
   validates :state, :city, :demand, :sleep, :friend, :smoke, :department, :music, :presence => true
-  
+
   validates :state, :city, :format => {
     :with => /\A[a-zA-Z\u00c7\u00e7\u011e\u011f\u0130\u0131\u00d6\u00f6\u015e\u015f\u00dc\u00fc]{1,20}\z/
   }
-  
+
   has_one :advert, :as => :advertable
-  
+
   enum :demand => [ :male, :female, :both ]
-  
+
   enum :sleep => [ :evet1, :hayir1, :degisken1 ]
   enum :friend => [ :evet2, :hayir2, :degisken2 ]
   enum :smoke => [ :evet3, :hayir3, :arada3 ]
   enum :department => [ :muhendislik, :egitim, :tip, :siyasal, :diger ]
   enum :music => [ :evet4, :hayir4, :arada4 ]
-
-
   def aranan
-    
+
     if self.demand == 'male'
-      
+
       return 'Erkek'
-      
+
     elsif self.demand == 'female'
-      
-      return 'Kadın' 
-      
-   
-    
+
+      return 'Kadın'
+
     elsif self.demand == 'both'
-      
-      return 'Hepsi' 
-      
+
+      return 'Hepsi'
+
     end
-    
-    
+
   end
 
+  def uyuma
+
+    if self.sleep == 'evet1'
+
+      return "Evet"
+
+    elsif self.sleep == 'hayir1'
+
+      return "Hayır"
+
+    elsif self.sleep == 'degisken1'
+
+      return "Değişken"
+
+    end
+
+  end
+
+  def arkadas
+
+    if self.friend == 'evet2'
+
+      return "Evet"
+
+    elsif self.friend == 'hayir2'
+
+      return "Hayır"
+
+    elsif self.friend == 'degisken2'
+
+      return "Değişken"
+
+    end
+
+  end
+
+  def sigara
+
+    if self.smoke == 'evet3'
+
+      return "Evet"
+
+    elsif self.smoke == 'hayir3'
+
+      return "Hayır"
+
+    elsif self.smoke == 'degisken3'
+
+      return "Değişken"
+
+    end
+
+  end
+
+  def muzik
+
+    if self.music == 'evet4'
+
+      return "Evet"
+
+    elsif self.music == 'hayir4'
+
+      return "Hayır"
+
+    elsif self.music == 'degisken4'
+
+      return "Değişken"
+
+    end
+
+  end
 
 end
--- app/models/privatelesson.rb
+++ app/models/privatelesson.rb
@@ -27,4 +27,52 @@ class Privatelesson < ActiveRecord::Base
 
   end
 
+  def ozelderssekli
+
+    if self.kind == 'birebir'
+
+      return "Bire Bir"
+
+    elsif self.kind == 'grup'
+
+      return "Grup"
+
+    elsif self.kind == 'sinifdersi'
+
+      return "Sınıf Dersi"
+
+    end
+
+  end
+
+  def derskategori
+
+    if self.lecture == "matematik"
+
+      return "Matematik"
+
+    elsif self.lecture == "fizik"
+
+      return "Fizik"
+
+    elsif self.lecture == "kimya"
+
+      return "Kimya"
+
+    elsif self.lecture == "biyoloji"
+
+      return "Biyoloji"
+
+    elsif self.lecture == "genelmuhendislik"
+
+      return "Genel Mühendislik"
+
+    elsif self.lecture == "genelegitimbilimleri"
+
+      return "Genel Eğitim Bilimleri"
+
+    end
+
+  end
+
 end
