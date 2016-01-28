2634a62a65110af165e50daea39e36d02e9efdea
oguz <oguz.tanrikulu@metu.edu.tr>
2016-01-28 22:29:30 +0200

    captcha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  require "uri"
+  require "net/http"
+  
   helper_method :topUniversities
   
   before_filter :check_admin_mode,
