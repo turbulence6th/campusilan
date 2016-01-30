59b97c9ad208c6af5fd65e97911a42543130b463
oguz <oguz.tanrikulu@metu.edu.tr>
2016-01-30 15:37:56 +0200

    selo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,8 @@ class ApplicationController < ActionController::Base
 
   def check_admin_mode
     if ENV['ADMIN_MODE'] && (request.remote_ip != "144.122.120.18" &&
-       request.remote_ip != "144.122.75.31" && request.remote_ip != "144.122.120.164")
+       request.remote_ip != "144.122.75.31" && request.remote_ip != "144.122.120.164" &&
+       request.remote_ip != "185.40.73.252")
       redirect_to '/portfolio'
     end
   end
