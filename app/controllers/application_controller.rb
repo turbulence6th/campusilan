40521ff5b33ef36ca46591825dc87fd4059285a3
oguz <oguz.tanrikulu@metu.edu.tr>
2016-01-31 22:59:09 +0200

    ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < ActionController::Base
     if ENV['ADMIN_MODE'] && (request.remote_ip != "144.122.120.18" &&
        request.remote_ip != "144.122.75.31" && request.remote_ip != "144.122.120.164" &&
        request.remote_ip != "185.40.73.252" && request.remote_ip != "78.177.158.196" && request.remote_ip != "144.122.150.67" &&
-       request.remote_ip != "95.70.132.224" && request.remote_ip != "103.1.210.26" && request.remote_ip !="78.170.135.229" && request.remote_ip !="83.66.150.149")
+       request.remote_ip != "95.70.132.224" && request.remote_ip != "103.1.210.26" && request.remote_ip !="78.170.135.229" && request.remote_ip !="83.66.150.149" &&
+       request.remote_ip != "144.122.131.27")
       redirect_to '/portfolio'
     end
   end
