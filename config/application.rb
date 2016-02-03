ac77d7f2fa1cc7d2f9d6a0f628cfd55498ee29e1
oguz <oguz.tanrikulu@metu.edu.tr>
2016-02-03 18:16:50 +0200

    AS

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Campusilan
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.use Rack::Attack
-    config.middleware.use Rack::Deflater
     config.exceptions_app = self.routes
     # Raises error for missing translations
     # config.action_view.raise_on_missing_translations = true
